2d89aeb680f007f3bcfb60b49dc3657107a8356f
rrohm <r.rohm@aeonium-systems.de>
2024-01-11 22:11:47 +0100

    Bumped copyright/license year to 2024.

--- main/Gruntfile.js
+++ main/Gruntfile.js
@@ -1,7 +1,7 @@
 /* 
  * The MIT License
  *
- * Copyright 2022 Robert Rohm r.rohm@aeonium-systems.de.
+ * Copyright 2024 Robert Rohm r.rohm@aeonium-systems.de.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
